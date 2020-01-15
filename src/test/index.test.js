b9829d4d442aee750e2e99c077775a8debe93783
WangJyYan <wangjuyan@58.com>
2020-01-15 15:23:53 +0800

    test coverage

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -3,3 +3,7 @@ const fun = require('../main/index1.js');
 test('adds 1 + 2 to equal 3', () => {
   expect(fun.sum(1, 2)).toBe(3);
 });
+
+test('subs 5 - 2 to equal 3', () => {
+  expect(fun.sub(5, 2)).toBe(3);
+});
