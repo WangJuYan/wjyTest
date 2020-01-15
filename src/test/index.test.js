6010006046bc44e88817180f746dd73217804222
WangJyYan <wangjuyan@58.com>
2020-01-15 15:40:18 +0800

    test coverage

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -4,6 +4,6 @@ test('adds 1 + 2 to equal 3', () => {
   expect(fun.sum(1, 2)).toBe(3);
 });
 
-test('subs 5 - 2 to equal 3', () => {
-  expect(fun.sub(5, 2)).toBe(3);
-});
+// test('subs 5 - 2 to equal 3', () => {
+//   expect(fun.sub(5, 2)).toBe(3);
+// });
