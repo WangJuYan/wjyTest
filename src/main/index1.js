04be72f28e9517265972f6f500f56b5c53b891ea
wangjuyan <wangjuyan@58.com>
2020-05-23 15:40:12 +0800

    modify test

--- src/main/index1.js
+++ src/main/index1.js
@@ -1,13 +1,14 @@
 function sum(a, b) {
-  // return a + b;
-  if (a > b) {
-    return 'a - b';
-  } else {
-    return a + b;
-  }
+  return a + b;
 }
 
 function sub (a, b) {
-  return a - b;
+  if (a > 0 && b > 0) {
+    return a - b;
+  } else if (a < 0 || b < 0) {
+    return a + b;
+  } else {
+    return 0
+  }
 }
 module.exports = {sum, sub};
--- src/test/index.test.js
+++ src/test/index.test.js
@@ -1,13 +1,17 @@
 const fun = require('../main/index1.js');
 
-test('adds 1 + 2 to equal 3', () => {
+test('sum', () => {
   expect(fun.sum(1, 2)).toBe(3);
 });
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(fun.sum(5, 2)).toBe('a - b');
+test('test1: a>0，b>0:', () => {
+  expect(fun.sub(5, 2)).toBe(3);
 });
 
-test('subs 5 - 2 to equal 3', () => {
-  expect(fun.sub(5, 2)).toBe(3);
+test('test2: a<0', () => {
+  expect(fun.sub(-2, 5)).toBe(3);
+});
+
+test('test3: a=0', () => {
+  expect(fun.sub(0, 5)).toBe(0);
 });
